84995c47e8d62ef081a2f583b73471bb68f92328
daurenpelmen8656 <155034944+daurenpelmen8656@users.noreply.github.com>
2026-09-12 18:33:40 +0500

    Delete EmailBuilder.java

--- EmailBuilder.java
+++
@@ -1,7 +0,0 @@
-public interface EmailBuilder {
-    EmailBuilder setFrom(String from);
-    EmailBuilder addRecipient(String recipient);
-    EmailBuilder setSubject(String subject);
-    EmailBuilder setBody(String body);
-    EmailBuilder addAttachment(String fileName);
-}
